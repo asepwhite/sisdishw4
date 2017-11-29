1b7b26909caf1cbfc4b7ba7480ee692f6dd211d4
asepwhite <septriyan.akbar@gmail.com>
2017-11-29 18:33:51 +0700

    update

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 var amqp = require('amqplib/callback_api');
 // amqp://sisdis:sisdis@172.17.0.3:5672
 amqp.connect('amqp://sisdis:sisdis@172.17.0.3:5672', function(err, conn) {
-  console.log(err)
   console.log(conn)
-  console.log("wwkwkwk");
   conn.createChannel(function(err, ch) {
+    console.log("berhasil create channel")
     var ex = 'EX_PING';
     var msg = process.argv.slice(2).join(' ') || 'Hello World!';
 
